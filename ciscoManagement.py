9f6207f0f03d46591aed91419973372f1a40b79b
bjamesdowning <bjamesdowning@gmail.com>
2017-03-22 07:42:37 -0500

    Update ciscoManagement.py

--- ciscoManagement.py
+++ ciscoManagement.py
@@ -17,25 +17,28 @@ outputFilename = input('Enter Destination Filename: ')
 username = input('Username: ')
 password = getpass.getpass(prompt='Password: ', stream=None)
 
-
+#Function to generate a list based on commands found in a text file within the same directory. Need to make this
+#dynamic for use of config files from anywhere. 
 def get_commands(cmdFilename):
     with open(cmdFilename, 'r') as cmd:
         cmdLines = cmd.read()
         cmdList = cmdLines.split('\n')
     return cmdList
 
+#Function to generate list of devices from a text file, easy to pull from csv listing line by line
 def get_devices(deviceFilename):
     with open(deviceFilename, 'r') as deviceIP:
         deviceIPLines = deviceIP.read()
         deviceIPList = deviceIPLines.split('\n')
     return deviceIPList
 
+#Function to build device dictionary for Netmiko, also calls get_devices and push_config
 def device_dict(deviceFilename,username,password):
-
+    #calls get_devices to obtain list of device IP's
     ipAddressList = (get_devices(deviceFilename))
     try:
         if (ipAddressList[0].lower()) == 'asa':
-            ipAddressList.remove(ipAddressList[0])
+            ipAddressList.remove(ipAddressList[0]) #Using the first line in the device doc to dictate the OS, then remove it - Find better way
 
             for IP in ipAddressList:
                 ASA = {
@@ -78,6 +81,8 @@ def device_dict(deviceFilename,username,password):
     except Exception as ValueError:
         print(ValueError)
 
+#Function is called by device_dict. Calls get_commands to obtain list, then uses already established ssh socket to run commands
+#Need to incorporate more dynamic way or determining if the command is needed to be ran in config or exec mode
 def push_config(ssh_conn):
 
     cmdList = (get_commands(cmdFilename))
@@ -87,6 +92,7 @@ def push_config(ssh_conn):
     except Exception as ValueError:
         print(ValueError)
 
+#Function to send config or output to a file. More so to confirm the config I'm putting in each device
 def device_output(result):
     try:
         outputFile = open(outputFilename, 'a')
